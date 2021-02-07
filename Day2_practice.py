b83c1ee555b4343f520b80586a47888a0fbe0fe1
RobinJLin <dreamhopeandfaith@gmail.com>
2021-02-08 02:21:47 +0800

    update HW

--- Day2_practice.py
+++ Day2_practice.py
@@ -1,12 +1,10 @@
 # Day 2 
 import numpy as np
-#see python shell 
-
 ##1.將下列清單(list1)，轉成維度為(5X6)的array，順序按列填充。(hint:order="F")
 array1 = np.array(range(30))
 print(array1)
 
-print(array1.reshape(5,6))
+print(array1.reshape(5,6).ravel(order = "F"))
 
 #2.呈上題的array，找出被6除餘1的數的索引
 
